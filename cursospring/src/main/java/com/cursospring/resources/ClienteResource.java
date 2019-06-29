7041e2f1ff32596c68427658a7349c9871932852
jpedropmont <jpedropmont@gmail.com>
2019-06-29 16:18:24 -0300

    Endpoint clientes

---
+++ cursospring/src/main/java/com/cursospring/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.cursospring.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursospring.domain.Cliente;
+import com.cursospring.services.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find (@PathVariable Integer id) { //@PathVariable traduz o id da requisição para o parametro da função
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
