24939d2babeb3ab2e00718b23605f261d20928ae
jpedropmont <jpedropmont@gmail.com>
2019-07-23 19:38:43 -0300

    Inserindo uma nova Categoria com POST

--- cursospring/src/main/java/com/cursospring/resources/CategoriaResource.java
+++ cursospring/src/main/java/com/cursospring/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.cursospring.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursospring.domain.Categoria;
 import com.cursospring.services.CategoriaService;
@@ -21,6 +26,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find (@PathVariable Integer id) { //@PathVariable traduz o id da requisição para o parametro da função
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert (@RequestBody Categoria obj) { // @RequestBody converte o json para um objeto como a Categoria.
+		obj = service.inserir(obj); // O repositório retorna um objeto do tipo inserido, e por isso armazenamos esse objeto retornado na variável 'obj'.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
